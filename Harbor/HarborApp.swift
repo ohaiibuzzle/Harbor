bd829391537a57fd41b9f1ce214f33b62d0640b0
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2023-06-07 16:56:17 +0700

    Initial Commit

---
+++ Harbor/HarborApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarborApp.swift
+//  Harbor
+//
+//  Created by Venti on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct HarborApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
