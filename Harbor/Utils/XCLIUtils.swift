b5de046c37a00bfd1b8747785030b9ab2b317cc2
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2023-06-09 21:21:46 +0700

    updated: stricter Xcli checks

--- Harbor/Utils/XCLIUtils.swift
+++ Harbor/Utils/XCLIUtils.swift
@@ -11,9 +11,13 @@ struct XCLIUtils {
     static let shared = XCLIUtils()
     
     func checkXcliInstalled() -> Bool {
+        // SANITY: Check if /Library/Developer/CommandLineTools exists
+        if !FileManager.default.fileExists(atPath: "/Library/Developer/CommandLineTools") {
+            return false
+        }
+
         // pkgutil --pkg-info=com.apple.pkg.CLTools_Executables | grep version
         // Looking for 'version: 15.0.0.0.1.1685693485'
-
         let task = Process()
         task.launchPath = "/usr/sbin/pkgutil"
         task.arguments = ["--pkg-info=com.apple.pkg.CLTools_Executables"]
